91301fce1b184823f3303cfdb9236e24d546c377
Dave Carpenter <davvcarpenter@gmail.com>
2018-11-16 00:26:08 -0600

    added qr scanner, redirect, advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20181101214427) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
