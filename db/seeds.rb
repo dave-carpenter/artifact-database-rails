ef6ac9444f33422d3481e9937f351a58681141cf
Josh Razalan <jawau56@gmail.com>
2018-12-12 01:27:35 -0600

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+artifact_ex = [
+  [ "Husky Ladder", "failed rung", "Construction", "California" ],
+  [ "18ft Ladder", "failing slide mechanism", "Construction", "Illinois" ],
+  [ "Industrial Dishwasher", "Heating element exploded", "Fire", "Nebraska" ],
+  [ "Cummins Bus Engine", "rod failure", "Automotive", "Illinois" ],
+  [ "Train", "Litrally a whole train with suspected engine failure", "Rail", "Illinois" ]
+]
+artifact_ex.each do|name, desc, dept, loc|
+    Artifact.create( name: name, description: desc, department: dept, default_location: loc)
+end
